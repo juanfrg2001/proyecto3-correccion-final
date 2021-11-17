d5c8c6eb790b3affd7763bfd63b5d1492335bcfd
juanf <jfruizg@unbosque.edu.co>
2021-11-17 10:25:48 -0500

    Update User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attr_accessor :reset_token
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: FILL_IN, reset_sent_at: FILL_IN)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+
 end
