69c053068196f3c8afb6cc545748dcf75416f8a8
juanf <jfruizg@unbosque.edu.co>
2021-11-17 08:44:13 -0500

    Update User

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'proyecto3hogaruc.herokuapp.com'
+  host = 'https://proyecto3hogaruc.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
