fd125f941c2761f40415ca9d7acc1ce9f937e75e
juanf <jfruizg@unbosque.edu.co>
2021-11-22 12:51:02 -0500

    Update chart

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bulma"
-
+import "chartkick/chart.js"
 
 Rails.start()
 Turbolinks.start()
