fadc4f25b885cf909b2e58d0592571e5a6239b50
juanf <jfruizg@unbosque.edu.co>
2021-11-28 12:50:58 -0500

    Update

--- app/models/calory.rb
+++ app/models/calory.rb
@@ -3,7 +3,7 @@ class Calory < ActiveRecord::Base
   scope :having_dob_between, ->(start_date, end_date) { where(dob: start_date..end_date) }
   self.inheritance_column = nil
 
-  enum type: [:burned, :gained]
+  enum type_calory: [:burned, :gained]
 
 
 end
