ad0bf6369ea559b1cb3d0fecd55502182d0e970e
juanf <jfruizg@unbosque.edu.co>
2021-11-28 10:35:41 -0500

    update

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,8 +4,8 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-    @user = current_user.create_reset_digest
-    @user = current_user.send_password_reset_email
+    current_user.create_reset_digest
+    current_user.send_password_reset_email
     flash[:info] = "Email sent with password reset instructions"
     redirect_to root_url
   end
