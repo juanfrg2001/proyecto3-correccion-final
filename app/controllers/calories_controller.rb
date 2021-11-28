c289f3c361d7cfcc858415e83b499d51718de141
juanf <jfruizg@unbosque.edu.co>
2021-11-28 16:47:52 -0500

    update

--- app/controllers/calories_controller.rb
+++ app/controllers/calories_controller.rb
@@ -1,6 +1,6 @@
 class CaloriesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  before_action :set_calory, only: %i[ show edit update destroy chart ]
+  before_action :set_calory, only: %i[ show edit update destroy ]
 
 
   def index
