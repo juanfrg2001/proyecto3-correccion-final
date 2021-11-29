1c850b64f329b35fd59805c66e7e7a538d9e54ef
juanf <jfruizg@unbosque.edu.co>
2021-11-28 23:13:10 -0500

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'password_resets/edit'
   root 'calories#index'
   get 'chart', to: 'calories#chart'
-  get 'result', to: 'weights#result'
   resources :calories
   resources :weights
   resources :password_resets
